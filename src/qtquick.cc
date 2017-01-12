40e8203068af8b8eba4ec941453de3f4b82f9fb0
Ryohei Ikegami <iofg2100@gmail.com>
2017-01-12 15:48:25 +0900

    Test Qt with qDebug

--- src/qtquick.cc
+++ src/qtquick.cc
@@ -1,12 +1,14 @@
 #include <nan.h>
+#include <QDebug>
 
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
+    qDebug() << "method called";
+    info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
 }
 
 void Init(v8::Local<v8::Object> exports) {
-  exports->Set(Nan::New("hello").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(Method)->GetFunction());
+    exports->Set(Nan::New("hello").ToLocalChecked(),
+                 Nan::New<v8::FunctionTemplate>(Method)->GetFunction());
 }
 
 NODE_MODULE(hello, Init)
